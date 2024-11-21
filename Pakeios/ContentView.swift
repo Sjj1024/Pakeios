b2f3dcbf620dcb431382f068ea041e7c33f4ac18
1024小神 <15670339118@163.com>
2024-11-21 19:06:41 +0800

    init

---
+++ Pakeios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pakeios
+//
+//  Created by song on 2024/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello Pakeios!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pakeios/PakeiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PakeiosApp.swift
+//  Pakeios
+//
+//  Created by song on 2024/11/21.
+//
+
+import SwiftUI
+
+@main
+struct PakeiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
